ab79480d60a889506513e343fd46bb14bb4f2b6c
ToymanShikhar <shikharverma043@gmail.com>
2021-10-19 02:37:08 +0530

    Admin dashboard done

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -15,7 +15,12 @@ import {
     ORDER_LIST_MY_RESET,
     ORDER_LIST_REQUEST,
     ORDER_LIST_SUCCESS,
-    ORDER_LIST_FAIL
+    ORDER_LIST_FAIL,
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_RESET,
+    ORDER_DETAILS_RESET
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -42,6 +47,8 @@ export const orderDetailsReducer = (
             return { loading: false, order: action.payload };
         case ORDER_DETAILS_FAIL:
             return { loading: false, error: action.payload };
+        case ORDER_DETAILS_RESET:
+            return { loading: true, orderItems: [], shippingAddress: {} };
         default:
             return state;
     }
@@ -56,6 +63,21 @@ export const orderPayReducer = (state = {}, action) => {
         case ORDER_PAY_FAIL:
             return { loading: false, error: action.payload };
         case ORDER_PAY_RESET:
+            return {};
+        default:
+            return state;
+    }
+};
+
+export const orderDeliverReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_DELIVER_REQUEST:
+            return { loading: true };
+        case ORDER_DELIVER_SUCCESS:
+            return { loading: false, success: true };
+        case ORDER_DELIVER_FAIL:
+            return { loading: false, error: action.payload };
+        case ORDER_DELIVER_RESET:
             return {};
         default:
             return state;
