d7f579219c60d07e22acad0e6158d7759d0569c3
ToymanShikhar <shikharverma043@gmail.com>
2021-10-22 20:35:04 +0530

    Add Search Bar  in Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,6 +33,7 @@ const App = () => {
                     <Route path="/register" component={RegisterScreen} />
                     <Route path="/profile" component={ProfileScreen} />
                     <Route path="/" component={HomeScreen} exact />
+                    <Route path="/search/:keyword" component={HomeScreen} />
                     <Route path="/product/:id" component={ProductScreen} />
                     <Route path="/admin/userlist" component={UserListScreen} />
                     <Route
